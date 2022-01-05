da253063e5df7ad31007a32ae9f35f3f6370c6ca
mshukla-intel <mangalx.shukla@intel.com>
2022-01-05 16:06:57 +0530

    updated the script

--- timescript.sh
+++ timescript.sh
@@ -1,12 +1,5 @@
 #! /bin/bash
-
-sleeptime=$SLEEPTIME
-end=$((SECONDS + (sleeptime * 60)))
-while [ $SECONDS -lt $end ]; do
-    echo "Time Now: `date +%H:%M:%S`"
-    echo "Gone to sleep mode for" $end "seconds" # Do what you want.
-    sleep 30
-    echo "Time Now: `date +%H:%M:%S`"
-done;
+echo "Going to sleep for $SLEEPTIME"
+sleep $SLEEPTIME m
 echo "sleep time completed"
 exit 0;
